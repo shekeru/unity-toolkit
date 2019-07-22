66e7b49889324ad71424a32ffcb0da6df9c765a7
Sheky Sheks <sheks@desu.systems>
2019-07-22 14:47:15 -0400

    rocket boots

--- src/BeanBattles.Assembly/Cheat.cs
+++ src/BeanBattles.Assembly/Cheat.cs
@@ -8,16 +8,24 @@ namespace BeanAssembly
     {
         private void OnGUI()
         {
-            GUI.Label(new Rect(10, 10, 200, 40), "Niggerhook v0.1~"); int loc = 60;
-            var serverManager = GameObject.Find("Server Manager").GetComponent<ServerManager>();
-            var gameManager = GameObject.Find("gameManager").GetComponent<GameManager>();
-            foreach (var player in serverManager.playersList) {
-                GUI.Label(new Rect(45, loc+=40, 200, 40), player.name);
+            GUI.Label(new Rect(10, 10, 200, 40), "Niggerhook v0.2~"); int loc = 60;
+            //var serverManager = GameObject.Find("Server Manager").GetComponent<ServerManager>();
+            var gameManager = GameObject.Find("gameManager").GetComponent<GameManager>();;
+            foreach (var player in gameManager.players) {
+                try {
+                    var movement = player.GetComponent<Movement>();
+                    var local = player.GetComponent<SetUpLocalPlayer>();
+                    if (movement.enabled && !local.isSpectating)
+                        GUI.Label(new Rect(45, loc += 40, 200, 40), 
+                            local.pname);
+                    if (movement.isLocalPlayer)
+                    {
+                        // Enable Rocket Boots
+                        movement.rocketJumpEnabled = true;
+                        movement.boostPower = 100f;
+                    }           
+                } catch { }
             }
-        }
-        public void Update()
-        {
-            
         }
     }
 }
